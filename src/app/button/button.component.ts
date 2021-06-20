8b40886337700381777939b3541e825781f2cb1d
SrManinka <gbaalmeida@hotmail.com>
2021-06-20 14:49:22 -0300

    button module declaration

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gaac-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
