bf02927b2a56842f90a6d6450a056c92e098cc3f
SrManinka <gbaalmeida@hotmail.com>
2021-06-20 15:02:46 -0300

    button module inside the right place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
 @NgModule({
     declarations: [
         AppComponent,
-        ButtonComponent,
     ],
     imports: [
         BrowserModule,
